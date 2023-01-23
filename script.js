4980eeeaf3a0b408477cf57495a107cb8c45d8aa
Yogesh Kumar Dangi <yogeshdangi0722@gmail.com>
2023-01-23 11:16:06 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+'use strict';
+
+// prettier-ignore
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+const form = document.querySelector('.form');
+const containerWorkouts = document.querySelector('.workouts');
+const inputType = document.querySelector('.form__input--type');
+const inputDistance = document.querySelector('.form__input--distance');
+const inputDuration = document.querySelector('.form__input--duration');
+const inputCadence = document.querySelector('.form__input--cadence');
+const inputElevation = document.querySelector('.form__input--elevation');
+
+if(navigator.geolocation){
+    navigator.geolocation.getCurrentPosition(
+        (position)=>{
+        console.log(position);
+        const {latitude,longitude}= position.coords;
+        console.log(`https://www.google.com/maps/@${latitude},${longitude},15z`);
+        const map = L.map('map').setView([latitude, longitude], 13);
+
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
+
+        L.marker([latitude, longitude]).addTo(map)
+            .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+            .openPopup();
+            
+    }
+    ,
+    ()=>{
+        console.log("Could not get current position");
+    }
+    )
+}
